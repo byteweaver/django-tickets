18952ecda5bd8564495241ade63cf9923f7eca36
akuryou <akuryou@akuryou.net>
2012-09-24 19:05:37 +0200

    add test for Ticket model

---
+++ tickets/tests/models_tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+from tickets.tests.factories import UserFactory, TicketFactory
+
+
+class TicketTestCase(TestCase):
+    def test_model(self):
+        obj = TicketFactory()
+        self.assertTrue(obj.pk)
+
+    def test_is_closed(self):
+        obj = TicketFactory()
+        self.assertFalse(obj.is_closed())
+        obj.status = 4
+        self.assertTrue(obj.is_closed())
+
