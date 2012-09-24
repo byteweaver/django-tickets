12183e4fbd50712378d7f9898594611d3804b887
akuryou <akuryou@akuryou.net>
2012-09-24 23:23:28 +0200

    use sequence in user factory

--- tickets/tests/factories.py
+++ tickets/tests/factories.py
@@ -7,6 +7,8 @@ from tickets.models import Ticket
 class UserFactory(factory.Factory):
     FACTORY_FOR = User
 
+    username = factory.Sequence(lambda n: "User %s" % n)
+
 class TicketFactory(factory.Factory):
     FACTORY_FOR = Ticket
     
