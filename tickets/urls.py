d69668a3f4ed38dd0908db9694a6e7aafe112f29
noxan <noxan@byteweaver.net>
2012-12-19 10:04:09 +0100

    add login_required decorator to my list view url

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth.decorators import login_required
 
 from views import MyTicketListView
 
 urlpatterns = patterns('',
-    url(r'^my/$', MyTicketListView.as_view(), name='list'),
+    url(r'^my/$', login_required(MyTicketListView.as_view()), name='list'),
 )
