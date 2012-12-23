2491a33d7dc06ef708a8cf70bde45f03ba665ab2
noxan <noxan@byteweaver.net>
2012-12-23 18:01:50 +0100

    add url for ticket create view

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 
-from views import MyTicketListView, MyTicketDetailView
+from views import MyTicketListView, MyTicketDetailView, TicketCreateView
 
 urlpatterns = patterns('',
     url(r'^my/$', login_required(MyTicketListView.as_view()), name='list'),
     url(r'^my/(?P<pk>\d+)/$', login_required(MyTicketDetailView.as_view()), name='detail'),
+    url(r'^create/$', login_required(TicketCreateView.as_view()), name='create'),
 )
