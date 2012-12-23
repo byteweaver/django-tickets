433ab981ef35dd2c89ea43ee26566eb5d67449bf
noxan <noxan@byteweaver.net>
2012-12-23 18:02:18 +0100

    rework ticket create view and use custom form

--- tickets/views.py
+++ tickets/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView, CreateView
+from django.core.urlresolvers import reverse
 
+from forms import TicketCreateForm
 from models import Ticket
 
 
@@ -18,5 +20,12 @@ class MyTicketDetailView(DetailView):
         return Ticket.objects.filter(creator=self.request.user)
 
 class TicketCreateView(CreateView):
-    model = Ticket
+    form_class = TicketCreateForm
     template_name = 'tickets/ticket_create.html'
+
+    def form_valid(self, form):
+        ticket = form.instance
+        ticket.creator = self.request.user
+        ticket.save()
+        self.success_url = reverse('tickets:detail', args=[ticket.id])
+        return super(TicketCreateView, self).form_valid(form)
