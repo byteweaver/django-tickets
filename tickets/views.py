bacb45a345e20c7d52eff8a1407ab1bdd626c763
noxan <noxan@byteweaver.net>
2012-12-23 23:03:39 +0100

    improve my ticket detail view to handle post requests with creating ticket comments

--- tickets/views.py
+++ tickets/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import ListView, DetailView, CreateView
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 
-from forms import TicketCreateForm
+from forms import TicketCreateForm, TicketCommentCreateForm
 from models import Ticket
 
 
@@ -15,6 +16,25 @@ class MyTicketListView(ListView):
 class MyTicketDetailView(DetailView):
     model = Ticket
     template_name = 'tickets/ticket_mydetail.html'
+    form = TicketCommentCreateForm()
+
+    def get_context_data(self, **kwargs):
+        context = super(MyTicketDetailView, self).get_context_data(**kwargs)
+        context['form'] = self.form
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.form = TicketCommentCreateForm(request.POST)
+        if self.form.is_valid():
+            self.form_valid(self.form)
+        return super(MyTicketDetailView, self).get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.instance
+        comment.author = self.request.user
+        comment.ticket = Ticket.objects.get(id=self.kwargs['pk'])
+        comment.save()
+        messages.info(self.request, 'Ticket comment created.')
 
     def get_queryset(self):
         return Ticket.objects.filter(creator=self.request.user)
