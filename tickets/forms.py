fb4f535961f1d43636de78333d342a0d073105a3
noxan <noxan@byteweaver.net>
2012-12-23 23:02:50 +0100

    implement ticket comment create form

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,9 +1,14 @@
 from django import forms
 
-from models import Ticket
+from models import Ticket, TicketComment
 
 
 class TicketCreateForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ('subject', 'description')
+
+class TicketCommentCreateForm(forms.ModelForm):
+    class Meta:
+        model = TicketComment
+        fields = ('comment',)
