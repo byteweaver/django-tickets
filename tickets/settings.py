5eba5f3a3cb17441619016ed5e50c57f1f778f8c
noxan <noxan@byteweaver.net>
2013-02-10 17:09:12 +0100

    remove unnecessary indention from tickets settings

--- tickets/settings.py
+++ tickets/settings.py
@@ -9,7 +9,7 @@ STATUS_CHOICES = getattr(settings, 'TICKETS_STATUS_CHOICES', (
     (3, _("In Progress")),
     (4, _("Resolved")),
     (5, _("Closed")),
-    ))
+))
 
 # List of statuses that define a ticket as finally closed
 CLOSED_STATUSES = getattr(settings, 'TICKETS_CLOSED_STATUSES', (4,5))
