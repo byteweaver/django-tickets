adcd5160620e96a9ec3d86e8d620e34bd3ce7be9
akuryou <akuryou@byteweaver.net>
2014-06-20 20:41:49 +0200

    use DjangoModelFactory for tests

--- tickets/tests/factories.py
+++ tickets/tests/factories.py
@@ -4,17 +4,17 @@ import factory
 from tickets.models import Ticket, TicketComment
 
 
-class UserFactory(factory.Factory):
+class UserFactory(factory.DjangoModelFactory):
     FACTORY_FOR = User
 
     username = factory.Sequence(lambda n: "User %s" % n)
 
-class TicketFactory(factory.Factory):
+class TicketFactory(factory.DjangoModelFactory):
     FACTORY_FOR = Ticket
-    
+
     creator = factory.LazyAttribute(lambda a: UserFactory())
 
-class TicketCommentFactory(factory.Factory):
+class TicketCommentFactory(factory.DjangoModelFactory):
     FACTORY_FOR = TicketComment
 
     ticket = factory.LazyAttribute(lambda a: TicketFactory())
