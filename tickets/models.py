84fdb0a852791a9bd5081c76343e9dba137045ca
akuryou <akuryou@byteweaver.net>
2014-06-20 20:31:49 +0200

    use AUTH_USER_MODEL settings, not User model

--- tickets/models.py
+++ tickets/models.py
@@ -1,18 +1,18 @@
-from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from tickets import settings
+from tickets.settings import STATUS_CHOICES, CLOSED_STATUSES
 
 
 class Ticket(models.Model):
-    creator = models.ForeignKey(User, verbose_name=_("Creator"), related_name='tickets')
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Creator"), related_name='tickets')
     date = models.DateTimeField(_("Date"), auto_now_add=True)
     last_update = models.DateTimeField(_("Date"), auto_now=True)
     subject = models.CharField(_("Subject"), max_length=255)
     description = models.TextField(_("Description"), help_text=_("A detailed description of your problem."))
-    assignee = models.ForeignKey(User, verbose_name=_("Assignee"), related_name="assigned_tickets", blank=True, null=True)
-    status = models.SmallIntegerField(_("Status"), choices=settings.STATUS_CHOICES, default=0)
+    assignee = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Assignee"), related_name="assigned_tickets", blank=True, null=True)
+    status = models.SmallIntegerField(_("Status"), choices=STATUS_CHOICES, default=0)
 
     class meta:
         verbose_name = _("Ticket")
@@ -29,7 +29,7 @@ class Ticket(models.Model):
         return "%s# %s" % (self.id, self.subject)
 
     def is_closed(self):
-        return self.status in settings.CLOSED_STATUSES
+        return self.status in CLOSED_STATUSES
 
     def is_answered(self):
         try:
@@ -44,7 +44,7 @@ class Ticket(models.Model):
 class TicketComment(models.Model):
     ticket = models.ForeignKey(Ticket, verbose_name=_("Ticket"), related_name='comments')
     date = models.DateTimeField(auto_now_add=True, verbose_name=_("Date"), editable=False)
-    author = models.ForeignKey(User, verbose_name=_("Author"))
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Author"))
     comment = models.TextField(_("Comment"))
 
     class Meta:
