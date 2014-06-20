f30d2d842e78a883ea6b8a00412c00e615e1d3c8
akuryou <akuryou@byteweaver.net>
2014-06-20 20:32:22 +0200

    use raw id fields for users

--- tickets/admin.py
+++ tickets/admin.py
@@ -6,10 +6,12 @@ from models import Ticket, TicketComment
 class TicketCommentInline(admin.StackedInline):
     model = TicketComment
     extra = 1
+    raw_id_fields = ['author']
 
 
 class TicketAdmin(admin.ModelAdmin):
     model = Ticket
+    raw_id_fields = ['creator', 'assignee']
     inlines = [TicketCommentInline, ]
 
 
